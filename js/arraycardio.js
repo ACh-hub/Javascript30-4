c1b00b764e03f4e110c85632cf4989f1eb5edd77
Aleksandra Chrapkowska <amchrapkowska@gmail.com>
2019-01-17 19:24:02 +0100

    another sort done

--- js/arraycardio.js
+++ js/arraycardio.js
@@ -15,8 +15,15 @@ const inventors = [
 
 const people = ['Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick', 'Beecher, Henry', 'Beethoven, Ludwig', 'Begin, Menachem', 'Belloc, Hilaire', 'Bellow, Saul', 'Benchley, Robert', 'Benenson, Peter', 'Ben-Gurion, David', 'Benjamin, Walter', 'Benn, Tony', 'Bennington, Chester', 'Benson, Leana', 'Bent, Silas', 'Bentsen, Lloyd', 'Berger, Ric', 'Bergman, Ingmar', 'Berio, Luciano', 'Berle, Milton', 'Berlin, Irving', 'Berne, Eric', 'Bernhard, Sandra', 'Berra, Yogi', 'Berry, Halle', 'Berry, Wendell', 'Bethea, Erin', 'Bevan, Aneurin', 'Bevel, Ken', 'Biden, Joseph', 'Bierce, Ambrose', 'Biko, Steve', 'Billings, Josh', 'Biondo, Frank', 'Birrell, Augustine', 'Black, Elk', 'Blair, Robert', 'Blair, Tony', 'Blake, William'];
 
+
+const dashboard = document.querySelector('.dashboard');
+
 // Create inventors div content
-const inventorsDiv = document.getElementById('inventors');
+const inventorsDiv = document.createElement('div');
+inventorsDiv.classList.add('data');
+inventorsDiv.id ='inventors';
+dashboard.appendChild(inventorsDiv);
+
 // Array.prototype.map()
 // 2. Give us an array of the inventors' first and last names
 const inventorsMapped = inventors.map(inventor=>`${inventor.first} ${inventor.last}`);
@@ -26,33 +33,77 @@ inventorsMapped.forEach(inventor=>{
     let textNode = document.createTextNode(`${inventor}`);
     spanElement.appendChild(textNode);
     inventorsDiv.appendChild(spanElement);
-    
 });
+
+
 // Array.prototype.filter()
 // 1. Filter the list of inventors for those who were born in the 1500's
 inventors1500 = inventors.filter(inventor => (inventor.year>=1500 && inventor.year<1600));
 
-const fifteensDiv = document.getElementById('fifteens');
+
+const fifteensDiv = document.createElement('div');
+fifteensDiv.classList.add('data');
+fifteensDiv.id ='fifteens';
+dashboard.appendChild(fifteensDiv);
 fifteensDiv.appendChild(createTable(inventors1500));
 
 // Array.prototype.sort()
 // 3. Sort the inventors by birthdate, oldest to youngest
 inventorsSorted = inventors.sort((first,last)=>{return first.year -last.year});
 
-const sortedDiv = document.getElementById('sorted');
+const sortedDiv = document.createElement('div');
+sortedDiv.classList.add('data');
+sortedDiv.id ='sorted';
+dashboard.appendChild(sortedDiv);
 sortedDiv.appendChild(createTable(inventorsSorted));
 
 // Array.prototype.reduce()
 // 4. How many years did all the inventors live?
 
+const howMany = inventors.reduce((total, current)=>{
+    let currentYears = current.passed - current.year;
+    return total+currentYears;
+},0);
+
+const howManyDiv = document.createElement('div');
+howManyDiv.classList.add('data');
+howManyDiv.id ='how-many';
+dashboard.appendChild(howManyDiv);
+
+howManyDiv.innerHTML=`<span>The inventors lived ${howMany} years.</span>`;
+
 // 5. Sort the inventors by years lived
+inventorsYearsSorted = inventors.sort((first,last)=>{
+    let firstAge = first.passed - first.year;
+    let lastAge = last.passed - last.year;
+    return firstAge -lastAge;
+});
 
-// 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
-// https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
-
+const sortedYearsDiv = document.createElement('div');
+sortedYearsDiv.classList.add('data');
+sortedYearsDiv.id ='years-sorted';
+dashboard.appendChild(sortedYearsDiv);
+sortedYearsDiv.appendChild(createTable(inventorsYearsSorted));
 
 // 7. sort Exercise
 // Sort the people alphabetically by last name
+
+const peopleSorted = people.sort((first,next)=>{
+    return first.split(", ")[0]-next.split(", ")[0];
+});
+
+
+const peopleSortedDiv = document.createElement('div');
+peopleSortedDiv.classList.add('data');
+peopleSortedDiv.id ='people-sorted';
+dashboard.appendChild(peopleSortedDiv);
+
+peopleSorted.forEach(person=>{
+    let spanElement = document.createElement("span");
+    let textNode = document.createTextNode(`${person}`);
+    spanElement.appendChild(textNode);
+    peopleSortedDiv.appendChild(spanElement);
+});
 
 // 8. Reduce Exercise
 // Sum up the instances of each of these
@@ -79,7 +130,6 @@ function createTable(arr){
             cell.innerHTML = el[key];
         }
     });
-        console.log(Object.keys(arr[0]));
-
+    
         return table;
 }
